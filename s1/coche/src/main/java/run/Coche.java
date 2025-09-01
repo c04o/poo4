afaaf7bcbaaad04f1396330eee5c97dc01519ed6
c04o <c04o@proton.me>
2025-09-01 15:45:19 -0600

    Add Coche class

---
+++ s1/coche/src/main/java/run/Coche.java
@@ -0,0 +1,13 @@
+package run;
+
+public class Coche {
+        String marca;
+        String modelo;
+        int anio;
+
+        public String mostrarDatos()
+        {
+            return String.format("Marca: %s, Modelo: %s, Año: %d.", marca, modelo, anio);
+        }
+    }
+}
