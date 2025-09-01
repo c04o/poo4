2034ff615cd2e2321677cb9f7bda97133f12303a
c04o <c04o@proton.me>
2025-09-01 17:03:29 -0600

    Implement Estudiante's methods in Main

--- s1/estudiante/src/main/java/run/Main.java
+++ s1/estudiante/src/main/java/run/Main.java
@@ -1,17 +1,20 @@
 package run;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.ArrayList;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+    public static void main(String[] args)
+    {
+        ArrayList<Estudiante> estudiantes = new ArrayList<Estudiante>();
+        estudiantes.add(new Estudiante("Valeria", "IND0414", 60.0));
+        estudiantes.add(new Estudiante("Connie", "SIS0205", 65.5));
+        estudiantes.add(new Estudiante("Dan", "SIS0202", 100.0));
+
+        for(Estudiante estudiante : estudiantes)
+        {
+            /* Mostrar datos del estudiante */
+            System.out.println("Datos del estudiante: " + estudiante.mostrarDatos());
+            System.out.println( (estudiante.aprobado()) ? "Aprobó" : "Reprobó");
         }
     }
 }
