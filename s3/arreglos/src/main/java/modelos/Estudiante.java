98053001b1aa51607ace22a52f4c345dda07b73b
c04o <c04o@proton.me>
2025-09-08 18:11:55 -0600

    Create modelos package and add Estudiantes class to it

---
+++ s3/arreglos/src/main/java/modelos/Estudiante.java
@@ -0,0 +1,27 @@
+package modelos;
+
+public class Estudiante {
+    private int edad;
+    private String nombre;
+
+    public Estudiante(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+}
